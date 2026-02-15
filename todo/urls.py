a3c318816ef4458741108f7832d8b0752a067521
luis <luislemosmsb@gmail.com>
2026-02-15 19:36:13 +0000

    todo app final

--- todo/urls.py
+++ todo/urls.py
@@ -3,4 +3,17 @@ from .import views
 
 urlpatterns = [
     path('addTask/', views.addTask, name='addTask'),
+    # Mark as done 
+    path('mask_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+
+    # Mark as unfinished 
+    path('mark_as_unfinished/<int:pk>/', views.mark_as_unfinished, name='mark_as_unfinished'),
+
+    # edit task
+
+    path('edit_task/<int:pk>/', views.edit_task, name = 'edit_task'),
+
+    # delete task 
+    path('delele_task/<int:pk>/', views.delete_task, name = 'delete_task'),
+
 ]
--- todo_main/views.py
+++ todo_main/views.py
@@ -4,8 +4,10 @@ from todo.models import Task
 
 def home(request):
    tasks = Task.objects.filter(is_completed= False).order_by('-updated_at')
+   completed_at = Task.objects.filter(is_completed= True)
    context = {
       'tasks': tasks,
+      'completed':completed_at,
    }
   
    return render(request, 'home.html', context)
